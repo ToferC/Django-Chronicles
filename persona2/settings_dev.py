33357dafde59e81ae758e9e2ae669f574812441b
Cgeist7 <cgeist7@gmail.com>
2015-04-11 09:38:44 -0400

    Update for Heroku deployment

--- persona2/settings_dev.py
+++ persona2/settings_dev.py
@@ -154,21 +154,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Heroku Settings
-
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    )
