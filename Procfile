c9bc0241fe5de8088cc8438556512b55a3c83db5
Cgeist7 <cgeist7@gmail.com>
2015-04-14 21:37:40 -0400

    Update 4 for Heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn persona2.wsgi --log-file -
+web: waitress-serve --port=5000 persona2.wsgi:application
 
--- persona2/wsgi.py
+++ persona2/wsgi.py
@@ -11,6 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "persona2.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
