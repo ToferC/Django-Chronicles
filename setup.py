28ae7952b198d052e7e47ca88da41b9666752613
Cgeist7 <cgeist7@gmail.com>
2015-04-25 19:14:15 -0400

    Fixed packaging for app

--- setup.py
+++ setup.py
@@ -6,4 +6,18 @@ setup(name='Django Chronicles',
     author='Chris Allison',
     author_email='cgeist7@gmail.com',
     url='https://github.com/Cgeist7/Django-Chronicles',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers, Storytellers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
 )
