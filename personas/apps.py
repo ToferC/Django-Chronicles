3b304ad9ccfa818546c9356a2ad5737e0ea89ab2
Cgeist7 <cgeist7@gmail.com>
2015-04-25 20:59:31 -0400

    tweak to Appconfig

--- personas/apps.py
+++ personas/apps.py
@@ -6,4 +6,4 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 class PersonasConfig(AppConfig):
     name = 'personas'
     verbose_name = "Personas"
-    path = os.path.join(BASE_DIR, 'personas')
+    path = os.path.join(BASE_DIR,)
