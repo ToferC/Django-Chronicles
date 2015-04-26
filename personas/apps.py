122240cd313cc1970e60aed6be0b58dcf4571694
Cgeist7 <cgeist7@gmail.com>
2015-04-25 20:08:15 -0400

    Test on appconfig error

--- personas/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-class PersonasConfig(AppConfig):
-    name = 'personas'
-    verbose_name = "Personas"
-    path = os.path.join(BASE_DIR, 'personas')
