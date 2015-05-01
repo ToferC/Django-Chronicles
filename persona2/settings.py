9ee576f46f27045ff53d83241e6f6768c4fe5cbd
Cgeist7 <cgeist7@gmail.com>
2015-05-01 12:38:04 -0400

    Upgrade to Django 1.8
    Issues with current versions of sorl-thumbnails, bootstrap and crispy
    forms - depreciating in Django 1.9

--- persona2/settings.py
+++ persona2/settings.py
@@ -51,10 +51,22 @@ else:
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # insert your TEMPLATE_DIRS here
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
-            # ... some options here ...
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
         },
     },
 ]
